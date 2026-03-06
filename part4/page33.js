e4815cc69aa90667fcb8211a1c06fa3dcda3329a
Ande Abhilash <andeabhilash123@gmail.com>
2026-03-06 11:20:11 +0530

    Initial MERN project upload

---
+++ part4/page33.js
@@ -0,0 +1,10 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+const result = add(5, 3);
+console.log(result);
+const result2 = subtract(5, 3);
+console.log(result2);
---
+++ part4/page34.js
@@ -0,0 +1,20 @@
+ import express from "express"
+const app = express()
+app.set("view engine","ejs")
+app.set("views","views")
+app.listen(8080, ()=> console.log("Server Started"));
+
+const users = [
+    {name:"Karthik", email:"karthik@gmail.com", password: "1234"},
+    {name:"Komal", email:"komal@gmail.com", password: "1234"},
+    {name:"Karan", email:"karan@gmail.com", password: "1234"},
+];
+app.get("/login",(req,res)=> {
+    res.render("login");
+});
+app.get("/register",(req,res)=> {
+    res.render("register");
+});
+app.get("/",(req,res)=> {
+    res.render("dashboard",{users});
+});
